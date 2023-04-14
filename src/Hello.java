ae1dc1e7fac76acf9e4dc6bbf7c8658a42b22b0f
Geekomo <meerim210@gmail.com>
2023-04-14 17:28:59 +0200

    Second line of code in Hello

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
-
+        //commenting
+        System.out.println("Hello World 2");
+        System.out.println("Mac and Windows users are happy to see commit dialog");
     }
 
 }
