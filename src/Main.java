57711fb8115ba05d6698d80c43308dda28c8905f
Geekomo <meerim210@gmail.com>
2023-04-14 17:12:12 +0200

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("practicing a line of code in IntelliJ");
     }
 }
